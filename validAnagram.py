914aee1c20cf7544787c759fc3f6dc79234338be
Junead Khan <54591708+juneadkhan@users.noreply.github.com>
2021-10-07 14:16:20 -0400

    corrected validAnagram

--- validAnagram.py
+++ validAnagram.py
@@ -5,6 +5,10 @@ Input: s = "anagram", t = "nagaram"
 Output: true
 """
 
-# O(n) Solution:
+# O(nlog(n)) Solution:
 def isAnagram(s, t):
     return sorted(s) == sorted(t)
+
+# O(n) Solution:
+def isAnagram(s, t):
+    return Counter(s) == Counter(t)
